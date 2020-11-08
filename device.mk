c85038ca2fb8d9c159590cb25545436cee6d933e
dmd79 <davidevinavil@gmail.com>
2020-11-08 16:58:10 +0100

    I01WD: Move to qti BT stack

--- device.mk
+++ device.mk
@@ -66,6 +66,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.surface_flinger.supports_background_blur=1
 
+# Bluetooth
+PRODUCT_PACKAGES += \
+    BluetoothQti
+
 # Boot control
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.0-impl.recovery \
