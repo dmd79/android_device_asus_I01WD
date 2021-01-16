dce126b9e0379717515da5f67ddb644ef1d3f887
dmd79 <davidevinavil@gmail.com>
2021-01-16 22:55:01 +0100

    I01WD: Build livedisplay

--- device.mk
+++ device.mk
@@ -276,9 +276,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/goodixfp.kl:system/usr/keylayout/goodixfp.kl \
     $(LOCAL_PATH)/keylayout/googlekey_input.kl:system/usr/keylayout/googlekey_input.kl
 
-# Media
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/media_profiles_vendor.xml:system/etc/media_profiles_vendor.xml
+# LiveDisplay
+PRODUCT_PACKAGES += \
+    lineage.livedisplay@2.0-service-sdm
 
 # Live Wallpapers
 PRODUCT_PACKAGES += \
@@ -286,6 +286,10 @@ PRODUCT_PACKAGES += \
     LiveWallpapersPicker \
     VisualizationWallpapers \
     librs_jni
+
+# Media
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/media_profiles_vendor.xml:system/etc/media_profiles_vendor.xml
 
 # NFC - NQ (NXP)
 PRODUCT_PACKAGES += \
